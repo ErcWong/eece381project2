ba48ccd0dee5cd6fc72944c18839a8be498cf28a
g10-vr <vishal.11.r@gmail.com>
2014-03-12 14:25:45 -0700

    Working landscape layout

--- Piano/src/com/example/piano/MainActivity.java
+++ Piano/src/com/example/piano/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.piano;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 	}
 
 	@Override
